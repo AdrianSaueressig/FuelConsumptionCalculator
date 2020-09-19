c8cc3c82956885f5e0a5ebb0b6b3706360363174
Adrian Saueressig <adrian.saueressig@gmail.com>
2020-09-19 15:17:38 +0200

    closes #6 implemented detailview for entries

--- app/src/main/java/com/nolari/benzinverbrauchberechner/fragment/DetailViewFragment.java
+++ app/src/main/java/com/nolari/benzinverbrauchberechner/fragment/DetailViewFragment.java
@@ -13,9 +13,13 @@ import com.nolari.benzinverbrauchberechner.R;
 import com.nolari.benzinverbrauchberechner.database.DatabaseSingleton;
 import com.nolari.benzinverbrauchberechner.database.TankEntry;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class DetailViewFragment extends Fragment{
 
     int uid = -1;
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
     public DetailViewFragment(int uid){
         this.uid = uid;
@@ -28,11 +32,34 @@ public class DetailViewFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_detailview, container, false);
 
         TankEntry tankEntry = DatabaseSingleton.getInstance(getContext()).getTankEntryDao().getEntry(uid);
-        // TODO fill detail view fields with data
-        TextView tv = v.findViewById(R.id.text3);
-        tv.setText(tankEntry.toString());
+
+        TextView tvHeader = v.findViewById(R.id.detailViewHeader);
+        TextView tvLitres = v.findViewById(R.id.detailView_litres);
+        TextView tvPpl = v.findViewById(R.id.detailView_pricePerLitre);
+        TextView tvKilometres = v.findViewById(R.id.detailView_kilometres);
+        TextView tvTripmeter = v.findViewById(R.id.detailView_tripmeter);
+        TextView tvConsumption = v.findViewById(R.id.detailView_fuelConsumption);
+
+        tvHeader.setText(
+                String.format(tvHeader.getText().toString(),
+                        sdf.format(tankEntry.getDate())));
+        tvLitres.setText(formatFloat2Decimals(tankEntry.getLitres(), "l"));
+        tvPpl.setText(formatFloat3Decimals(tankEntry.getPricePerLitre(), "€/l"));
+        tvKilometres.setText(formatFloat1Decimal(tankEntry.getKilometres(), "km"));
+        tvTripmeter.setText(formatFloat1Decimal(tankEntry.getTripmeter(), "km"));
+        tvConsumption.setText(formatFloat2Decimals(tankEntry.getFuelConsumption(), "l/100km"));
 
         return v;
+    }
+
+    private String formatFloat2Decimals(float toFormat, String unit) {
+        return String.format(Locale.GERMAN, "%.2f", toFormat) + " " + unit;
+    }
+    private String formatFloat1Decimal(float toFormat, String unit) {
+        return String.format(Locale.GERMAN, "%.1f", toFormat) + " " + unit;
+    }
+    private String formatFloat3Decimals(float toFormat, String unit) {
+        return String.format(Locale.GERMAN, "%.3f", toFormat) + " " + unit;
     }
 
 }
